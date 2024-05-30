e3cd3c5b1f0279dc5dff3bdb4fcb37733248e0cb
akshaymittha <92669974+akshaymittha@users.noreply.github.com>
2024-05-30 11:36:19 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while loop
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index : ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ["ironman", "batman", "thor"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value is : ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+let score = 11
+// do while loop
+do {
+    console.log(`score  is : ${score}`);
+    score++
+} while (score <= 10)
