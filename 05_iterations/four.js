292104014affca2e9e8c958b3dcc244f65e4b9c6
akshaymittha <92669974+akshaymittha@users.noreply.github.com>
2024-05-30 11:56:56 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,30 @@
+// for in loop
+
+const myObject = {
+    js: 'javascipt',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const program = ['js', 'ruby', 'java', 'cpp']
+
+for (const key in program) {
+    // console.log(program[key]);
+
+}
+
+// const map = new Map()
+// map.set('IND', "india")
+// map.set('USA', "united states")
+// map.set('FRA', "france")
+// map.set('IND', "india")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
