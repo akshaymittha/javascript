1874114af1b85f8121fe56a2332097bd0190454f
akshaymittha <92669974+akshaymittha@users.noreply.github.com>
2024-06-24 11:14:23 +0000

    call fuction in javascript

---
+++ 10_classes_oop/call.js
@@ -0,0 +1,15 @@
+function setUsername(username) {
+    // complex DB calls
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password) {
+    setUsername.call(this,username)
+
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("akshay", "aksay@gmail.com", "123")
+console.log(chai);
